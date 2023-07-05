ee6cd851f2fe2c464544164323b1e24d974f5ee5
Murilo Souza <murilo.souza@compliasset.com>
2023-07-05 17:58:58 -0300

    win

--- lib/home_controller.dart
+++ lib/home_controller.dart
@@ -3,6 +3,10 @@ import 'package:esc_pos_printer/esc_pos_printer.dart';
 import 'package:esc_pos_utils/esc_pos_utils.dart';
 import 'dart:io';
 import 'custom_printer.dart';
+import 'raw.dart';
+import 'dart:ffi';
+import 'package:ffi/ffi.dart';
+import 'package:win32/win32.dart';
 
 class HomeController {
   Danfe? parseXml(String xml) {
@@ -19,34 +23,40 @@ class HomeController {
       required PaperSize paper,
       required CapabilityProfile profile}) async {
     DanfePrinter danfePrinter = DanfePrinter(paper);
-    List<int> _dados = await danfePrinter.bufferDanfe(danfe, mostrarMoeda: false);
+    List<int> _dados =
+        await danfePrinter.bufferDanfe(danfe, mostrarMoeda: false);
     if (Platform.isLinux) {
       File file = File('/dev/usb/lp0');
       await file.writeAsBytes(_dados);
-      
-      ProcessResult result = await Process.run('lp', ['-d', '/dev/usb/lp0', file.path]);
-      
+
+      ProcessResult result =
+          await Process.run('lp', ['-d', '/dev/usb/lp0', file.path]);
+
       if (result.exitCode == 1) {
         print('File sent to printer successfully.');
       } else {
         print('Error sending file to printer.');
         print(result.stderr);
       }
-    } else if(Platform.isWindows) {
+    } else if (Platform.isWindows) {
       print('plataforma não suporta');
     } else {
       print('não suporta');
     }
   }
 
-  printCustomLayout({Danfe? danfe, required PaperSize paper, required CapabilityProfile profile}) async {
+  printCustomLayout(
+      {Danfe? danfe,
+      required PaperSize paper,
+      required CapabilityProfile profile}) async {
     final CustomPrinter custom = CustomPrinter(paper);
     List<int> _dados = await custom.bufferDanfe(danfe);
 
     if (Platform.isLinux) {
       File file = File('/dev/usb/lp0');
       await file.writeAsBytes(_dados);
-      ProcessResult result = await Process.run('lp', ['-d', '/dev/usb/lp0', file.path]);
+      ProcessResult result =
+          await Process.run('lp', ['-d', '/dev/usb/lp0', file.path]);
       print(result.exitCode);
       if (result.exitCode == 1) {
         print('File sent to printer successfully.');
@@ -54,8 +64,18 @@ class HomeController {
         print('Error sending file to printer.');
         print(result.stderr);
       }
-    } else if(Platform.isWindows) {
-      print('plataforma não suporta');
+    } else if (Platform.isWindows) {
+      const openCashDrawer = '\x1b\x70\x00';
+
+      using((Arena alloc) {
+        final printer = RawPrinter('EPSON TM-T20II Receipt', alloc);
+        final data = <String>[
+          ..._dados.map((byte) => String.fromCharCode(byte)),
+        ];
+        if (printer.printLines(data)) {
+          print('Success!');
+        }
+      });
     } else {
       print('não suporta');
     }
